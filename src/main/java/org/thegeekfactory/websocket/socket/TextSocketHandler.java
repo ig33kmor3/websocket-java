60410ce8452929ac2ce32bf28ca0dffc718e6243
Freddie Johnson Jr <31199155+ig33kmor3@users.noreply.github.com>
2021-09-03 19:30:33 -0700

    added multiple session support

--- src/main/java/org/thegeekfactory/websocket/socket/TextSocketHandler.java
+++ src/main/java/org/thegeekfactory/websocket/socket/TextSocketHandler.java
@@ -4,6 +4,8 @@ import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -15,6 +17,7 @@ public class TextSocketHandler extends TextWebSocketHandler {
     // establish connection with server
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
         super.afterConnectionEstablished(session);
     }
 
@@ -22,12 +25,22 @@ public class TextSocketHandler extends TextWebSocketHandler {
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         super.handleTextMessage(session, message);
-        session.sendMessage(message);
+        sessions.forEach(webSocketSession -> {
+            // only send message to others, not the originator
+            if (!webSocketSession.equals(session)) {
+                try {
+                    webSocketSession.sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     // disconnect connection from server
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
         super.afterConnectionClosed(session, status);
     }
 }
